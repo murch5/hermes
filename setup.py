4e1d70c271d417c945ff9882e2d7fcc07f4e4bda
murch <ryan.c.murchie@gmail.com>
2017-06-07 23:12:12 -0400

    add __init__ and setup .py for install of package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='io_util',
+    version='0.1.0',
+    packages=['io_util'],
+    url='',
+    license='',
+    author='Ryan',
+    author_email='',
+    description='Input/Output Utility functions', requires=['pandas']
+)
