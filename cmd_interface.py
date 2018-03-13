cce58844640c7a05492af5c7ee8a1ed6fdcdef5a
murch <ryan.c.murchie@gmail.com>
2018-03-13 00:32:28 -0400

    update basic cmd line interface for modules

--- cmd_interface.py
+++ cmd_interface.py
@@ -1,16 +1,25 @@
-#Imports
+# Imports
 import argparse
 import logging, logging.config
+import inspect
 import yaml
 import io
+import os
 import sys
+import socket
+import platform
 
-#Load base logging information
-loggerconfig_stream = open("logger.ini", "r")
+
+# Load base logging information
+PACKAGE_PATH = os.path.split(os.path.realpath(__file__))[0]
+
+loggerconfig_stream = open(PACKAGE_PATH + "/logger.ini", "r")
 log_config = yaml.safe_load(loggerconfig_stream)
 logging.config.dictConfig(log_config)
 
 logger = logging.getLogger(__name__)
+logging.Formatter.default_msec_format = '%s.%03d'
+
 
 class CmdInterface:
 
@@ -35,23 +44,25 @@ class CmdInterface:
 
         logger.debug("Creating argument parser")
         parser = argparse.ArgumentParser(prog=self.settings.get("prog"),
-                                                  description=self.settings.get("description"),
-                                                  epilog=self.settings.get("epilog"))
+                                         description=self.settings.get("description"),
+                                         epilog=self.settings.get("epilog"))
 
         argument_settings_list = self.settings.get("arguments")
 
         for argument in argument_settings_list:
-            print(argument)
-            parser.add_argument(argument.get("name"), dest=argument.get("dest"))
-
+            parser.add_argument(argument.get("name"), dest=argument.get("dest"), nargs=argument.get("nargs"))
 
         return parser
 
     def run(self, args):
 
         parsed_args = self.parse_args(args)
+        parsed_args_dict = vars(parsed_args)
 
-        print(parsed_args)
+        self.output_function_header()
+
+        self.func(**parsed_args_dict)
+
         pass
 
     def __init__(self, func, settings_path):
@@ -63,10 +74,19 @@ class CmdInterface:
         self.arg_parser = self.create_arg_parser()
         pass
 
-def forge():
-    pass
+    def output_function_header(self):
 
-c = CmdInterface(forge,"./settings.yml")
+        host = socket.gethostname()
+        py_version = platform.python_version()
+        plat = platform.platform(aliased=0, terse=0)
 
-if __name__ == "__main__":
-    c.run(sys.argv[1:])
+        logger.info("**************************************************")
+        logger.info(self.settings.get("glyph_l1") + "\t\t" + self.settings.get("project"))
+        logger.info(self.settings.get("glyph_l2") + "\t\t" + self.settings.get("prog"))
+        logger.info(self.settings.get("glyph_l3") + "\t\t" + "v " + self.settings.get("version"))
+        logger.info(self.settings.get("glyph_l4"))
+        logger.info("**************************************************")
+        logger.info("[host: " + host + ", version: Python " + py_version + "]")
+        logger.info("[platform: " + str(plat) + "]")
+
+        pass
